b76881dd4970e906546f1c7becda6f440d20024e
Stephen Paul Weber <singpolyma@singpolyma.net>
2014-09-27 11:17:32 -0500

    Better error for bitlength
    You never want leading zeros on an MPI anyway

--- OpenPGP/__init__.py
+++ OpenPGP/__init__.py
@@ -12,6 +12,8 @@ import itertools
 
 def bitlength(data):
     """ http://tools.ietf.org/html/rfc4880#section-12.2 """
+    if ord(data[0:1]) == 0:
+        raise OpenPGPException("Tried to get bitlength of string with leading 0")
     return (len(data) - 1) * 8 + int(floor(log(ord(data[0:1]), 2))) + 1
 
 def checksum(data):
